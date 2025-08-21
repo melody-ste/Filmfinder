c57d9881bd59c83252d60471dfb57994bc73160a
melody-ste <melody_stephan@icloud.com>
2025-08-21 15:39:25 +0200

    adding Vite to use .env to protect the api key

---
+++ filmfinder/src/main.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+// VARIABLES FORMULAIRE
+const form = document.getElementById("searchForm");
+const input = document.getElementById("searchInput");
+const resultsContainer = document.getElementById("results");
+
+// Clé API (venant de .env)
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+
+
