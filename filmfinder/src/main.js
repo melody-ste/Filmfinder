5e9081adebfb94132e9cd3622ee8cdda2d5b1c2d
melody-ste <melody_stephan@icloud.com>
2025-08-21 16:59:13 +0200

    functional read more modal with bootstrap

--- filmfinder/src/main.js
+++ filmfinder/src/main.js
@@ -37,13 +37,13 @@ function displayMovies(movies) {
 
   movies.forEach(movie => {
     const div = document.createElement("div");
-    div.classList.add("movie-card", "mb-3", "p-3", "border", "rounded");
+    div.classList.add("movie-card", "mb-3", "p-3", "border", "rounded", "bg-white");
 
     div.innerHTML = `
       <div class="d-flex align-items-start">
         <img src="${movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/150"}" 
             alt="${movie.Title}" 
-            style="max-width:150px; margin-right: 15px; border-radius:8px;" />
+            class="movie-poster"  />
         <div>
           <h3>${movie.Title}</h3>
           <p>${movie.Year}</p>
@@ -55,3 +55,31 @@ function displayMovies(movies) {
     resultsContainer.appendChild(div);
   });
 }
+
+
+async function fetchMovieDetails(id) {
+  const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}&plot=full`);
+  const data = await response.json();
+  return data;
+}
+
+// POP UP READ MORE
+resultsContainer.addEventListener("click", async (e) => {
+  if (e.target.classList.contains("readMore")) {
+    const movieId = e.target.dataset.id;
+    const details = await fetchMovieDetails(movieId);
+
+    document.getElementById("movieModalLabel").textContent = details.Title;
+    document.getElementById("movieDetails").innerHTML = `
+      ${details.Released}<br/><br/>
+      ${details.Genre}<br/><br/>
+      ${details.Plot}<br/><br/>
+      With: ${details.Actors}
+    `;
+
+    const modalEl = document.getElementById("movieModal");
+    const modal = new bootstrap.Modal(modalEl);
+    modal.show();
+  }
+});
+
